d63b9a1f20cb7eebdcd1f7c44c3051484a250ed9
Jeff Behrbaum <sejapresident@gmail.com>
2023-06-17 12:06:27 -0400

    Just making sure all the latest code in committed.

--- crowdfunding/src/TopLevelComponents/HomePage.tsx
+++ crowdfunding/src/TopLevelComponents/HomePage.tsx
@@ -1,9 +1,23 @@
 import './Styles/HomePageStyle.scss'
+import { useState } from 'react'
+import Modal from 'react-modal'
 
 type Props = {}
 
 export const HomePage = (props: Props) => {
+	const [showModal, setShowModal] = useState<boolean>(false)
+
+	const toggleDialog = () => {
+		setShowModal(!showModal)
+	}
+
 	return (
-		<div className='homepage'>HomePage</div>
+		<div className='homepage'>
+			<button onClick={() => toggleDialog()}>Toggle</button>
+			<Modal isOpen={showModal}>
+				<h1>Foo!!!</h1>
+				<button onClick={() => toggleDialog()}>bye-bye</button>
+			</Modal>
+		</div>
 	)
 }
